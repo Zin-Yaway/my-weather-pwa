18d2d9656651019c4723d0f3dafe7e79a24479dc
Zar Pyan Lay <zin.yaway@example.com>
2025-01-22 11:01:08 +0630

    Fixed line endings

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import { fetchWeather } from './api/fetchWeather.js';
+import './App.css';
+
+const App = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+    
+    const search = async (e) => {
+        if(e.key === 'Enter') {
+            const data = await fetchWeather(query);
+
+            setWeather(data);
+            setQuery('');
+        }
+    }
+
+    const headerStyle = {
+        fontFamily: 'Arial, sans-serif',
+        color: '#ffffff',
+        textAlign: 'center',
+        padding: '20px',
+        fontSize: '3rem'
+      };
+
+    return (
+        <div className="main-container">
+            <h1 style={headerStyle}>Weather App</h1>
+            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default App;
